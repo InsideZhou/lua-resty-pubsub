993f014062c6d703922dce6fcb654cddc1e30ab3
Inside <inside@insidezhou.me>
2017-11-30 14:34:09 +0800

    send close frame to client while ping frame threshold reached. close redis cosocket on main socket closed.

--- websocket.lua
+++ websocket.lua
@@ -6,6 +6,8 @@ local timeout = 5000
 local recv_threshold = 5 -- socket will be closed by server while recv_frame's continuous failure times exceeding this threshold, and a test ping is not answered by pong.
 local recv_errors = 0
 local test_ping_sent = false
+local test_ping_sendings = 0
+local test_ping_threshold = 5 -- socket will be closed while ping_frame sended times exceeding this threshold.
 
 local redis_host = "127.0.0.1"
 local redis_port = 6379
@@ -21,7 +23,7 @@ if not socket then
 end
 
 socket:set_timeout(timeout)
-ngx.log(ngx.DEBUG, "websocket established with timeout: ", timeout)
+ngx.log(ngx.DEBUG, "websocket established with timeout: ", timeout, ". remote_addr: ", ngx.var.remote_addr)
 
 local subscribe_co = nil
 local should_subscribe_stop = false
@@ -40,7 +42,7 @@ function subscribe(channels)
     local resp, err = cli:subscribe(unpack(channels))
     if not resp then
         ngx.log(ngx.ERR, "failed to subscribe channels ", cjson.encode(channels), ". : ", err)
-       return
+        return ngx.exit(444)
     end
 
     ngx.log(ngx.INFO, "channels subscribed: ", cjson.encode(resp))
@@ -52,7 +54,7 @@ function subscribe(channels)
 
         local resp, err = cli:read_reply()
         if not resp then
-            ngx.log(ngx.DEBUG, "no redis reply available.")
+            ngx.log(ngx.DEBUG, "no redis reply available. remote_addr: ", ngx.var.remote_addr)
             goto continue
         end
 
@@ -60,20 +62,29 @@ function subscribe(channels)
         if resp[1] == 'message' then
             local channel = resp[2]
             local msg = resp[3]
-            
-            local ws_resp, ws_err = socket:send_text(cjson.encode({ message=msg, channel=channel }))
+
+            local ws_resp, ws_err = socket:send_text(cjson.encode({ message = msg, channel = channel }))
             if not ws_resp then
                 ngx.log(ngx.ERR, "failed to send a text frame: ", ws_err)
             end
         end
 
-        ::continue::
+        :: continue ::
     end
 
-    local ok, err = cli:close()
+    local resp, err = cli:unsubscribe(unpack(channels))
+    if not resp then
+        ngx.log(ngx.ERR, "failed to unsubscribe channels ", cjson.encode(channels), ". : ", err)
+        cli.close()
+        return
+    end
+
+    local ok, err = cli:set_keepalive(timeout * 100, 10000)
     if not ok then
         ngx.log(ngx.ERR, "failed to close redis connection: ", err)
     end
+
+    ngx.log(ngx.DEBUG, "redis connection closed. remote_addr: ", ngx.var.remote_addr)
 end
 
 while true do
@@ -85,62 +96,62 @@ while true do
             break
         end
 
-        recv_errors = recv_errors + 1        
+        if not subscribe_co and test_ping_sendings > test_ping_threshold then
+            ngx.log(ngx.INFO, "test_ping_threshold exceeded. remote_addr: ", ngx.var.remote_addr, ", and not subscribing.")
+            break
+        end
+
+        recv_errors = recv_errors + 1
         if recv_errors > recv_threshold then
             socket:send_ping("ping")
+            test_ping_sendings = test_ping_sendings + 1
             test_ping_sent = true
         end
 
-        goto continute
+        goto continue
     end
 
     recv_errors = 0
 
-    if type == "close" then -- send a close frame back:
-        local bytes, err = socket:send_close(1000)
-        if not bytes then
-            ngx.log(ngx.ERR, "failed to send the 1000 close frame: ", err)
-            return
-        end
-
-        ngx.log(ngx.INFO, "closing with status code 1000.")
-        return
+    if type == "close" then
+        break
     end
 
-    if type == "ping" then -- send a pong frame back:
+    if type == "ping" then
+        -- send a pong frame back:
         local bytes, err = socket:send_pong(data)
         if not bytes then
             ngx.log(ngx.ERR, "failed to send a pong frame: ", err)
             break
         end
     elseif type == "pong" then
-        ngx.log(ngx.DEBUG, "a pong frame received")
+        ngx.log(ngx.DEBUG, "a pong frame received. remote_addr: ", ngx.var.remote_addr)
         test_ping_sent = false
     elseif type == "text" then
         local value, err = cjson.decode(data);
         if not value then
-            ngx.log(ngx.ERR, "failed to decode json: ", err)
-            goto continute
+            ngx.log(ngx.ERR, "failed to decode json: ", data, " err: ", err)
+            goto continue
         end
 
         local channels = value["channels"]
         if not channels or #channels == 0 then
             if not subscribe_co then
-                local resp, err = socket:send_text(cjson.encode({ code="1", msg="empty channels is not allowed." }))
+                local resp, err = socket:send_text(cjson.encode({ code = "1", msg = "empty channels is not allowed." }))
                 if not resp then
                     ngx.log(ngx.ERR, "failed to send a text frame: ", err)
                 end
             end
 
-            goto continute
+            goto continue
         end
-        
+
         if subscribe_co then
             should_subscribe_stop = true
             ngx.thread.wait(subscribe_co)
             should_subscribe_stop = false
         end
-        
+
         local err = nil
         subscribe_co, err = ngx.thread.spawn(subscribe, channels)
         if not subscribe_co then
@@ -151,14 +162,12 @@ while true do
         ngx.log(ngx.INFO, "received a frame of type ", type, " and payload ", data)
     end
 
-    ::continute::    
+    :: continue ::
 end
 
 if subscribe_co then
-    local ok, err = ngx.thread.kill(subscribe_co)
-    if not ok then
-        ngx.log(ngx.ERR, "failed to kill redis cosocket: ", err)
-    end
+    should_subscribe_stop = true
+    ngx.thread.wait(subscribe_co)
 end
 
 local bytes, err = socket:send_close()
@@ -166,3 +175,5 @@ if not bytes then
     ngx.log(ngx.ERR, "failed to send the close frame: ", err)
     return ngx.exit(444)
 end
+
+ngx.log(ngx.DEBUG, "websocket closed. remote_addr: ", ngx.var.remote_addr)
